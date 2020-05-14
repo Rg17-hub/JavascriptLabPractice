d72f4b39895a311728b9f9278b1a9b8f496c45fb
Rg17-hub <61347668+Rg17-hub@users.noreply.github.com>
2020-05-14 15:35:38 +0530

    Day 2 Assignments

---
+++ Day2/variables.js
@@ -0,0 +1,35 @@
+/*var tp=10;
+console.log(tp);
+console.log(typeof tp);
+console.log(50-70);
+console.log("Hello"+" "+"Hi");
+console.log(5 + "HI");
+console.log("Hi" + 5*20 + "bye");
+if(10==="10")
+console.log("good");
+else
+console.log("Bad");*/
+
+/*for(let i=0;i<10;i++)
+{
+    console.log(i);
+}
+console.log(i);*/
+
+
+function fun(a,b)
+{
+    console.log("Inside Function");
+}
+
+fun(5,0);
+fun("Hello", "Hi");
+var g=fun;
+g();
+
+var p= function()
+{
+    console.log("Inside anonymous function");
+}
+();
+
